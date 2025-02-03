7d52029ba44d0448363d12e817fcdaa8e488c949
Dheeraj Saraswat <dheeraj200410@gmail.com>
2025-02-03 22:09:54 +0530

    Minor changes

--- Ai-Extractor/src/App.jsx
+++ Ai-Extractor/src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <div className=" w-screen h-screen bg-blue-950 flex flex-col items-center text-white p-4 md:justify-center">
+    <div className="min-h-screen w-full bg-blue-950 flex flex-col items-center text-white p-4 md:justify-center">
       <div className="flex flex-col items-center md:mb-8">
         <Header />
       </div>
--- Ai-Extractor/src/components/Footer.jsx
+++ Ai-Extractor/src/components/Footer.jsx
@@ -2,9 +2,14 @@ import logo from "../assets/Gemini.png"
 
 function Footer() {
   return (
-    <div className="flex flex-col mt-4 text-center">
-      <p>
-        Powered by Google Gemini <img src={logo} alt="logo" className="mt-[-2rem]" width={190} />{" "}
+    <div className="flex flex-col items-center mt-4 text-center px-4">
+      <p className="flex flex-col items-center">
+        Powered by Google Gemini 
+        <img 
+          src={logo} 
+          alt="logo" 
+          className="w-[150px] md:w-[190px] mt-2" 
+        />
       </p>
     </div>
   );
